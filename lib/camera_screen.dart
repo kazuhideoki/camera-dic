d4be4a8332efb9e6ee5f09686e1c7d3b35f1a136
Kazuhide Oki <cedar123pc@gmail.com>
2020-11-24 17:53:09 +0800

    CameraScreenを別ファイルに

---
+++ lib/camera_screen.dart
@@ -0,0 +1,129 @@
+// import 'dart:html';
+import 'dart:io' show Directory;
+
+import 'package:camera/camera.dart'
+    show
+        CameraDescription,
+        CameraException,
+        availableCameras,
+        CameraController,
+        ResolutionPreset,
+        CameraPreview;
+import 'package:flutter/material.dart';
+import 'package:flutter_vision/image_detail.dart' show DetailScreen;
+import 'package:intl/intl.dart' show DateFormat;
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
+import 'package:firebase_core/firebase_core.dart' show Firebase;
+
+class CameraScreen extends StatefulWidget {
+  CameraScreen({Key key, @required this.cameras}) : super(key: key);
+
+  List<CameraDescription> cameras = [];
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(widget.cameras[0], ResolutionPreset.medium);
+    _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<String> _takePicture() async {
+    if (!_controller.value.isInitialized) {
+      print("Controller is not initialized");
+      return null;
+    }
+
+    // Formatting Date and Time
+    String dateTime = DateFormat.yMMMd()
+        .addPattern('-')
+        .add_Hms()
+        .format(DateTime.now())
+        .toString();
+
+    String formattedDateTime = dateTime.replaceAll(' ', '');
+    print("Formatted: $formattedDateTime");
+
+    final Directory appDocDir = await getApplicationDocumentsDirectory();
+    final String visionDir = '${appDocDir.path}/Photos/Vision\ Images';
+    await Directory(visionDir).create(recursive: true);
+    final String imagePath = '$visionDir/image_$formattedDateTime.jpg';
+
+    if (_controller.value.isTakingPicture) {
+      print("Processing is progress ...");
+      return null;
+    }
+
+    try {
+      await _controller.takePicture(imagePath);
+    } on CameraException catch (e) {
+      print("Camera Exception: $e");
+      return null;
+    }
+
+    return imagePath;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ML Vision'),
+      ),
+      body: _controller.value.isInitialized
+          ? Stack(
+              children: <Widget>[
+                // GetUserName('2kfcz2i9fjQ35TnUQhKl'),
+                CameraPreview(_controller),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    child: RaisedButton.icon(
+                      icon: Icon(Icons.camera),
+                      label: Text("Click"),
+                      onPressed: () async {
+                        await _takePicture().then((String path) {
+                          if (path != null) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailScreen(path),
+                              ),
+                            );
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                )
+              ],
+            )
+          : Container(
+              color: Colors.black,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:intl/intl.dart' show DateFormat;
 import 'package:path_provider/path_provider.dart'
     show getApplicationDocumentsDirectory;
 import 'package:firebase_core/firebase_core.dart' show Firebase;
+import 'camera_screen.dart' show CameraScreen;
 
 List<CameraDescription> cameras = [];
 
@@ -60,114 +61,6 @@ class MyApp extends StatelessWidget {
 class MyAppHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CameraScreen();
-  }
-}
-
-class CameraScreen extends StatefulWidget {
-  @override
-  _CameraScreenState createState() => _CameraScreenState();
-}
-
-class _CameraScreenState extends State<CameraScreen> {
-  CameraController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = CameraController(cameras[0], ResolutionPreset.medium);
-    _controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  Future<String> _takePicture() async {
-    if (!_controller.value.isInitialized) {
-      print("Controller is not initialized");
-      return null;
-    }
-
-    // Formatting Date and Time
-    String dateTime = DateFormat.yMMMd()
-        .addPattern('-')
-        .add_Hms()
-        .format(DateTime.now())
-        .toString();
-
-    String formattedDateTime = dateTime.replaceAll(' ', '');
-    print("Formatted: $formattedDateTime");
-
-    final Directory appDocDir = await getApplicationDocumentsDirectory();
-    final String visionDir = '${appDocDir.path}/Photos/Vision\ Images';
-    await Directory(visionDir).create(recursive: true);
-    final String imagePath = '$visionDir/image_$formattedDateTime.jpg';
-
-    if (_controller.value.isTakingPicture) {
-      print("Processing is progress ...");
-      return null;
-    }
-
-    try {
-      await _controller.takePicture(imagePath);
-    } on CameraException catch (e) {
-      print("Camera Exception: $e");
-      return null;
-    }
-
-    return imagePath;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ML Vision'),
-      ),
-      body: _controller.value.isInitialized
-          ? Stack(
-              children: <Widget>[
-                // GetUserName('2kfcz2i9fjQ35TnUQhKl'),
-                CameraPreview(_controller),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    child: RaisedButton.icon(
-                      icon: Icon(Icons.camera),
-                      label: Text("Click"),
-                      onPressed: () async {
-                        await _takePicture().then((String path) {
-                          if (path != null) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => DetailScreen(path),
-                              ),
-                            );
-                          }
-                        });
-                      },
-                    ),
-                  ),
-                )
-              ],
-            )
-          : Container(
-              color: Colors.black,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            ),
-    );
+    return CameraScreen(cameras: cameras);
   }
 }
